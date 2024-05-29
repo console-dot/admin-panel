21c7f02c1fddffed7978359fee138a059bce9026
Shuja Ur Rehman <shuja0094@gmail.com>
2024-05-29 12:59:55 +0500

    version condition

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,27 +1,31 @@
 const Response = require("../handlers/Response");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-class Auth extends Response{
-    authentication = (req,res, next)=>{
-        const authHeader = req.headers['authorization'];
-        const token= authHeader && authHeader.split(' ')[1];
+class Auth extends Response {
+  authentication = (req, res, next) => {
+    if (req.headers["version"] === "v2") {
+      next();
+    } else {
+      const authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
 
-        if(!token){
-            return this.sendResponse(req, res ,{
-                status:400,
-                message:"Un Authorized Request"
-            })
+      if (!token) {
+        return this.sendResponse(req, res, {
+          status: 400,
+          message: "Un Authorized Request",
+        });
+      }
+      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+          return this.sendResponse(res, err, {
+            status: 403,
+            message: "Forbidden",
+          });
         }
-        jwt.verify(token , process.env.JWT_SECRET, (err,user)=>{
-            if(err){
-                return this.sendResponse(res, err , {
-                    status:403,
-                    message:"Forbidden"
-                })
-            }
-            req.user =user;
-            next();
-        })
+        req.user = user;
+        next();
+      });
     }
+  };
 }
-module.exports = {Auth};
+module.exports = { Auth };
