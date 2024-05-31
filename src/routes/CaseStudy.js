907cec7d58559a4e71143317198c4d4c7c401eb8
Shuja Ur Rehman <shuja0094@gmail.com>
2024-05-31 18:57:53 +0500

    Update CaseStudy.js

--- src/routes/CaseStudy.js
+++ src/routes/CaseStudy.js
@@ -7,4 +7,6 @@ const auth = new Auth();
 router.get("/", auth.authentication, handlers.getAllCaseStudies);
 router.get("/:id", auth.authentication, handlers.getCaseStudy);
 router.post("/", auth.authentication, handlers.createCaseStudy);
+router.put("/:id", handlers.updateCaseStudy);
+router.delete("/:id", handlers.deleteCaseStudy);
 module.exports = router;
